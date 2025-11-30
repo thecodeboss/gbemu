135747fd0d21e228733334ce38e16b954e5ccb21
Michael Oliver <mikemboliver@gmail.com>
2025-11-30 02:37:22 -0800

    Fix client

--- apps/web/src/lib/supabase/client.ts
+++ apps/web/src/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createClient as createSupabaseClient } from "@supabase/supabase-js";
 
 export const supabase = createSupabaseClient(
   import.meta.env.VITE_SUPABASE_URL!,
-  import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
+  import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY!,
   {
     auth: {
       autoRefreshToken: true,
